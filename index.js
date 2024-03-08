a370d5e67bcd2410eead2eae6f78acb7363b5922
DevLung <123473305+DevLung@users.noreply.github.com>
2024-03-08 14:55:39 +0100

    serve html on /

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-const app = require("express")();
+const express = require("express");
+const app = express();
+const path = require("path");
+
 const PORT = 3000;
 
 
-app.listen(
-    PORT,
-    () => console.log(`DerLung-API listening on http://localhost:${PORT}`)
-);
 
+app.use(express.static(path.join(__dirname, "/html")))
+app.get(
+    "/",
+    (req, res) => {
+        res.status(200).sendFile("./html/index.html")
+    }
+)
 
 app.get(
     "/test",
     (req, res) => {
         res.status(200).send("test response");
     }
+);
+
+
+
+app.listen(
+    PORT,
+    () => console.log(`DerLung-API listening on http://localhost:${PORT}`)
 );
