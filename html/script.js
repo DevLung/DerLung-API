c9df8b461d9d2bdc7ab8dacd87c918f920d68a8e
DevLung <123473305+DevLung@users.noreply.github.com>
2024-03-13 16:23:33 +0100

    add actual website html

---
+++ html/script.js
@@ -0,0 +1,18 @@
+var copyUriButtonClicked = false;
+
+function copyUriToClipboard(button, uri) {
+    navigator.clipboard.writeText(uri);
+
+    if (!copyUriButtonClicked) {
+        copyUriButtonClicked = true;
+        var initialText = button.innerHTML;
+        var initialColor = button.style.backgroundColor
+        button.innerHTML = "URI kopiert!";
+        button.style.color = "#52b788"
+        setTimeout(function () {
+            button.innerHTML = initialText;
+            button.style.color = initialColor
+            copyUriButtonClicked = false;
+        }.bind(button), 1500);
+    }
+}
